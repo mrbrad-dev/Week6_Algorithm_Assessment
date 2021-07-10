ef272d1e11b53b8d2d81065910cdd2e3b7b2c6f5
mrbrad-dev <bradouyaiakpos@gmail.com>
2021-07-10 11:34:01 +0100

    Week6 Algorithm

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    println("True")
+}
+
+    object OppositesAttract {
+        fun isLove(
+            flower1: Int,
+            flower2: Int
+        ): Boolean {
+            return if (flower1 != flower2){
+                flower1 % 2 == 0 && flower2 % 2 != 0 || flower1 % 2 != 0 && flower2 == 0
+
+            } else {
+                false
+            }
+        }
+    }
